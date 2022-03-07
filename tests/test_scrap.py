a083caad3da77f650b0cd3f45da2b9060a611c7a
kramstyles <mark.eke@decagon.dev>
2022-03-07 18:36:58 +0100

    Created Skeleton for Test Cases

---
+++ tests/test_scrap.py
@@ -0,0 +1,31 @@
+from unittest import main, TestCase
+
+class TestGetData(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_validate_inputs(self):
+        pass
+
+    def test_for_soup_data(self):
+        pass
+
+class TestCleanData(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_to_check_for_characters(self):
+        pass
+
+    def test_to_check_for_common_words(self):
+        pass
+
+    def test_to_check_for_most_used_words(self):
+        pass
+
+class TestPlotData(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_for_data_plot(self):
+        pass
